aae443515a510f2a8c43d119ffef1c2f6cf83114
Shiva Sai <shivasaiyadav08@gmail.com>
2026-04-15 16:21:06 +0530

    configured backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: 'https://primetradeai-task.onrender.com',
+  origin: 'https://primetrade-ai-task-swart.vercel.app',
   credentials: true,
 }));
 
