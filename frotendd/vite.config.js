ea30fd308f94ada224c06b4c506169d2d5b4b889
Shiva Sai <shivasaiyadav08@gmail.com>
2026-04-15 15:43:48 +0530

    configured backend to frontend

--- frotendd/vite.config.js
+++ frotendd/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://primetradeai-task.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api/v1')
       }
