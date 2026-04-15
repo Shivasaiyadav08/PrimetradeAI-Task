2b11dc3560b5a5145ddc34a1bd20755492ac2e06
Shiva Sai <shivasaiyadav08@gmail.com>
2026-04-15 22:33:40 +0530

    added vercel.json file

--- frotendd/src/components/PrivateRoute.jsx
+++ frotendd/src/components/PrivateRoute.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
@@ -6,12 +6,12 @@ export default function PrivateRoute({ children }) {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return (
-      <div className="flex justify-center items-center h-64">
-        <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-600 border-t-transparent"></div>
-      </div>
-    );
+    return <div className="p-4">Loading...</div>;
   }
 
-  return user ? children : <Navigate to="/login" />;
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 }
